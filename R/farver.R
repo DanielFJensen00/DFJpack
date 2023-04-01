ce581ff10a1386a6ec0379512dae47de25b14625
DanielFJensen00 <Daniel.fisker.jensen@gmail.com>
2023-04-01 17:04:23 +0200

    Farve

--- R/farver.R
+++ R/farver.R
@@ -2,8 +2,30 @@
 #'
 #'
 #'
+#' @return Farvepalet
+#'
+#' @examples
+#' Kontrastfarve = KFUM_KONTRASTFARVER()
+#'
 
-KFUM_PRI = c("#71CC98","#1D3C34")
+
+KFUM_PRI <- function(){
+  KFUM_PRI = c("#71CC98","#1D3C34")
+  return(KFUM_PRI)
+}
+
+KFUM_SEKUND <- function(){
 KFUM_SEKUND = c("#007749","#169B62", "#AFA89D","#7A7D81","#D9D9D6")
-KFUM_KONTRASTFARVER = c("#500778","#FFD94A","#A85959","#FF9E1B","#DD5061")
-KFUM_SUPPLEMENT = c("#7D5787","#00C0AA","#C5003E","#DDCBA4","#63513D","#1295D8")
+return(KFUM_SEKUND)
+}
+
+KFUM_KONTRASTFARVER = function(){
+  KFUM_KONTRASTFARVER = c("#500778","#FFD94A","#A85959","#FF9E1B","#DD5061")
+  return(KFUM_KONTRASTFARVER)
+}
+
+KFUM_SUPPLEMENT = function(){
+  KFUM_SUPPLEMENT = c("#7D5787","#00C0AA","#C5003E","#DDCBA4","#63513D","#1295D8")
+  return(KFUM_SUPPLEMENT)
+}
+
