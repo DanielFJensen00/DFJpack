1737fb5f0dc49ade46250ddde9218de07ca61258
DanielFJensen00 <Daniel.fisker.jensen@gmail.com>
2023-04-10 16:28:20 +0200

    Data

--- R/data.R
+++ R/data.R
@@ -2,13 +2,15 @@
 #'
 #'
 #' Kortdata for kommuner
-#'
+#' @docType data
+#' @usage data(Kommunekort)
+#' @keywords datasets
 #'
 #' Time zone: Europe/Copenhagen
 #'
-#' @source \url{https://api.dataforsyningen.dk/kommuner?format=geojson}, hentet d. 10/04/2023
 #' @format A data frame with columns:
-#'  \describe{
+#'
+#'  \itemize{
 #'  \item{dagi_id}{Var for dagi_id}
 #'  \item{kode}{Var for kode}
 #'  \item{navn}{Var for navn}
@@ -17,12 +19,14 @@
 #'  \item{regionsnavn}{Var for regionsnavn}
 #'  \item{ændret}{Var for ændret}
 #'  \item{geo_ændret}{Var for geo_ændret}
-#'  \item{geo_version}{Var for geo_version}
+#'  \item{geo_version}{Var for geo_version}dev
 #'  \item{visueltcenter_x}{Var for visueltcenter_x}
 #'  \item{visueltcenter_y}{Var for visueltcenter_y}
 #'  \item{geometry}{Var for geometry}
 #' }
 #'
+#' @name Kommunekort
+#' @source \url{https://api.dataforsyningen.dk/kommuner?format=geojson}, hentet d. 10/04/2023
 #' @examples
 #' data(Kommunekort)
 "Kommunekort"
