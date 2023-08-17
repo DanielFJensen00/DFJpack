1ad2b9401599a68754fcda89e8cc6a474a43af98
DanielFJensen00 <Daniel.fisker.jensen@gmail.com>
2023-08-17 20:27:32 +0200

    plot

--- R/PLOTS.R
+++ R/PLOTS.R
@@ -7,9 +7,11 @@
 #' @param hsize description
 #'
 #' @details
-#' \describe{
-#' \item{`plot.donut()`}{.....}
+#'  \describe{
+#'    \item{`plot.donut()`}{.....}
 #'}
+#'
+#'
 #'
 #' @return A ggplot object.
 #'
@@ -17,15 +19,17 @@
 #' @importFrom ggplot2 ggplot geom_col facet_wrap coord_polar xlim geom_point theme element_blank element_rect coord_map geom_polygon aes update_geom_defaults geom_path ggtitle element_line element_rect element_text margin element_blank unit %+replace%
 #' @import tidyverse
 #'
-#' @name PLOTS
+#' @name Plots
 #' @aliases NULL
+NULL
 
 #' @export
 #' @title plot.donut
-#' @rdname PLOTS
+#' @rdname Plots
+#' @aliases NULL
 
 plot.donut <-  function(df,y,fill,hsize=3){
-  ggplot(df, aes(x=hsize, y = {{y}},fill={{fill}}))+
+  plot <- ggplot(df, aes(x=hsize, y = {{y}},fill={{fill}}))+
     geom_col(color="white", position = "fill") +
     coord_polar(theta = "y", start=0,clip="on")+
     xlim(c(0.2, hsize + 0.5))+
@@ -35,4 +39,5 @@ plot.donut <-  function(df,y,fill,hsize=3){
           axis.title = ggplot2::element_blank(),
           axis.ticks = ggplot2::element_blank(),
           axis.text = ggplot2::element_blank())
+  return(plot)
 }
