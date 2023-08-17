3a2ed9c9be68eab9d2dc5fcbbb9fe968af98f9d6
DanielFJensen00 <Daniel.fisker.jensen@gmail.com>
2023-08-17 20:41:11 +0200

    plot

--- R/PLOTS.R
+++ R/PLOTS.R
@@ -13,7 +13,7 @@
 #'
 #'
 #'
-#' @return A ggplot object.
+#' @return Et donut plot.
 #'
 #' @import ggplot2
 #' @importFrom ggplot2 ggplot geom_col facet_wrap coord_polar xlim geom_point theme element_blank element_rect coord_map geom_polygon aes update_geom_defaults geom_path ggtitle element_line element_rect element_text margin element_blank unit %+replace%
@@ -24,7 +24,7 @@
 NULL
 
 #' @export
-#' @title plot.donut
+#' @title Donut plot
 #' @rdname Plots
 #' @aliases NULL
 
@@ -32,12 +32,7 @@ plot_donut <-  function(df,y,fill,hsize=3){
   plot <- ggplot(df, aes(x=hsize, y = {{y}},fill={{fill}}))+
     geom_col(color="white", position = "fill") +
     coord_polar(theta = "y", start=0,clip="on")+
-    xlim(c(0.2, hsize + 0.5))+
-    theme(plot.background = ggplot2::element_rect("white"),
-          line = ggplot2::element_blank(),
-          panel.grid = ggplot2::element_blank(),
-          axis.title = ggplot2::element_blank(),
-          axis.ticks = ggplot2::element_blank(),
-          axis.text = ggplot2::element_blank())
+    xlim(c(0.2, hsize + 0.5))
+
   return(plot)
 }
